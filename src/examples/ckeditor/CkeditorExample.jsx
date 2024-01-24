621458291f346edaa9f420a361b08f8adea08955
Salva Mata <smr@nextret.net>
2024-01-24 13:25:40 +0100

    CKeditor: Editor basic funcionant amb mínims
    No permet afegir imatges des de la toolbar
    @todo intentar resoldre el problema

--- src/examples/ckeditor/CkeditorExample.jsx
+++ src/examples/ckeditor/CkeditorExample.jsx
@@ -1,11 +1,6 @@
 import React, {useState} from "react"
-import { CKEditor, CKEditorContext } from '@ckeditor/ckeditor5-react';
-
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-import { Context } from '@ckeditor/ckeditor5-core';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import Classiceditor from '@ckeditor/ckeditor5-build-classic';
 
 
 const CkeditorExample = () => {
@@ -21,37 +16,19 @@ const CkeditorExample = () => {
         setExemple(newExemple)
     }
 
+
     // Renderize
     return (
         <>
-            <CKEditorContext context={ Context }>
-                <h2>Using the CKEditor&nbsp;5 context feature in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    config={ {
-                        plugins: [ Paragraph, Bold, Italic, Essentials ],
-                        toolbar: [ 'bold', 'italic' ]
-                    } }
-                    data={exemple.content}
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor1 is ready to use!', editor );
-                    } }
-                />
-
-                <CKEditor
-                    editor={ ClassicEditor }
-                    config={ {
-                        plugins: [ Paragraph, Bold, Italic, Essentials ],
-                        toolbar: [ 'bold', 'italic' ]
-                    } }
-                    data={exemple.content}
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor2 is ready to use!', editor );
-                    } }
-                />
-            </CKEditorContext>
+            <h2>Using the CKEditor&nbsp;5 context feature in React</h2>
+            <CKEditor
+                editor = { Classiceditor }
+                name={"summary"}
+                data={ exemple.content}
+                onChange = { ( event, editor ) => {
+                    onCashange( editor );
+                }}
+            />
         </>
     )
 }
